e66ee24e7727b7463112588cec4176eb28fdd72b
s.barriendos.perez <s.barriendos.perez@avanade.com>
2024-02-16 17:27:30 +0100

    feat(presentation): Add Presentation Layer Middleware, Validators and Dependecy Injection

---
+++ scr/Presentation/Validators/PostValidator.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Presentation.Validators;
+public class PostValidator : AbstractValidator<Post>
+{
+    public PostValidator()
+    {
+        RuleFor(p => p.Content).NotEmpty().WithMessage("Content is required");
+    }
+}
